847540e00108f1850a8e282260cc992bb480ffee
Pororo99 <153965232+Pororo99@users.noreply.github.com>
2024-11-17 10:41:42 +0700

    Update no_proxy.py

--- no_proxy.py
+++ no_proxy.py
@@ -71,7 +71,7 @@ async def connect_to_wss(user_id):
 
 async def main():
     # TODO 修改user_id
-    _user_id = ''
+    _user_id = '2eqB5jrezu8zJGdAyTz5SHdaPYk'
     await connect_to_wss(_user_id)
 
 
